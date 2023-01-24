ea979e416350f687f4cbdac70eff419ed28deb9b
javawithlwl <javawithlwl@gmail.com>
2023-01-24 08:31:42 +0530

    Added biggest logic

--- src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
+++ src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
@@ -2,4 +2,18 @@ package com.careerit.jfs.basics.day7;
 
 public class BiggestElement {
 
+  public static void main(String[] args) {
+    int[] arr = new int[]{1, 2, 3, 4, 9, 8, 7, 6, 0, 9};
+    System.out.println(biggest(arr));
+  }
+
+  public static int biggest(int[] arr) {
+    int big = arr[0];
+    for (int ele : arr) {
+      if (big < ele) {
+        big = ele;
+      }
+    }
+    return big;
+  }
 }

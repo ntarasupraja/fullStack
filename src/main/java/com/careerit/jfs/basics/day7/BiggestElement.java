27f3b18302cfaa15252ae4d3bc3407586c4245aa
javawithlwl <javawithlwl@gmail.com>
2023-01-24 08:40:31 +0530

    Added smallest logic

--- src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
+++ src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
@@ -5,6 +5,7 @@ public class BiggestElement {
   public static void main(String[] args) {
     int[] arr = new int[]{1, 2, 3, 4, 9, 8, 7, 6, 0, 9};
     System.out.println(biggest(arr));
+    System.out.println(smallest(arr));
   }
 
   public static int biggest(int[] arr) {
@@ -15,5 +16,14 @@ public class BiggestElement {
       }
     }
     return big;
+  }
+  public static int smallest(int[] arr) {
+    int res = arr[0];
+    for (int i=1;i<arr.length;i++) {
+      if (res > arr[i]) {
+        res = arr[i];
+      }
+    }
+    return res;
   }
 }

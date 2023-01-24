ed14d8f5cefafb5bd43a5a1bb41b37b5e8f1d1bd
Tara <ntara1202@gmail.com>
2023-01-23 21:08:15 -0600

    Added new class for finding the smallest element

--- src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
+++ src/main/java/com/careerit/jfs/basics/day7/BiggestElement.java
@@ -5,6 +5,7 @@ public class BiggestElement {
   public static void main(String[] args) {
     int[] arr = new int[]{1, 2, 3, 4, 9, 8, 7, 6, 0, 9};
     System.out.println(biggest(arr));
+    System.out.println(smallest(arr));
   }
 
   public static int biggest(int[] arr) {
@@ -15,5 +16,14 @@ public class BiggestElement {
       }
     }
     return big;
+  }
+  public static int smallest(int[] arr) {
+    int small = arr[0];
+    for (int ele : arr) {
+      if (small > ele) {
+        small = ele;
+      }
+    }
+    return small;
   }
 }
